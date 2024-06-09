0a3dd7e4aadba0982d4452593b507ff1b7bc9cc8
caged-brush <88764977+caged-brush@users.noreply.github.com>
2024-06-08 20:36:57 -0700

    Delete client directory

--- client/src/components/CFL.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function CFL() {
-  return <h1>CFL</h1>;
-}
--- client/src/components/NBA.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useEffect, useState } from "react";
-
-export default function NBA() {
-  return;
-  <>
-    <h1>hhh</h1>
-  </>;
-}
